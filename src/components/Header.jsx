8d6448de88d0be82465cb506b899bea77c69c9df
Subha Ranjan <subharanjanbhowmick@gmail.com>
2024-01-16 12:36:11 +0530

    Initial Commit: Display Fetched Data

---
+++ src/components/Header.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import logoImg from '../assets/logo.jpg'
+
+export default function Header() {
+  return (
+    <header id='main-header'>
+        <div id='title'>
+            <img src={logoImg} alt='A Restuarant' />
+            <h1>React Food</h1>
+        </div>
+        <nav>
+            <button>Cart (0)</button>
+        </nav>
+    </header>
+  )
+}
