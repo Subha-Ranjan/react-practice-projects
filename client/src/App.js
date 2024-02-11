f8e0d61dc00cff2cc198861f2157ad042f60690e
Subha Ranjan <subharanjanbhowmick@gmail.com>
2024-02-11 16:58:15 +0530

    Frontend and backend implemented with Chatroom facility

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,37 @@
+import { useState,useEffect } from "react"
 
+import io from "socket.io-client"
+const socket = io.connect("http://localhost:3001/")
 
 function App() {
+  const [roomData, setRoomData] = useState("")
+  const [inputData, setInputData]=useState("")
+  const [messageReceived, setMessageReceived]=useState("")
+
+  function joinRoom(){
+    if(roomData!==""){
+      socket.emit("join_room",roomData)
+    }
+  }
+  function sendMessage(){
+    socket.emit("send_message", {message:inputData, room:roomData}) 
+  }
+  useEffect(()=>{
+    socket.on("received_message",(data)=>{
+      console.log("Received BroadCasted data:",data)
+      setMessageReceived(data.message)
+  })},[socket])
+
   return (
     <div className="App">
-     
+      <input placeholder="Room No..." onChange={(e) => {setRoomData(e.target.value)}}/>
+      <button onClick={joinRoom}> Join Room</button>
+
+      
+      <input placeholder="Message..." onChange={(e)=>{setInputData(e.target.value)}}/>
+      <button onClick={sendMessage} >Send Message</button>
+      <h1>Message Received:</h1><p>{messageReceived}</p>
     </div>
   );
 }
-
 export default App;
--- server/index.js
+++ server/index.js
@@ -13,19 +13,32 @@ const server = http.createServer(app)
 const cors = require('cors')
 app.use(cors())
 
-
 //io- anyting to do with socket-io with backnd
 //instance of Server Class 
 //const io = new Server(HTTPserver,{CORS info})
 const io = new Server(server,{
-    cors:{
-        origin:"http://localhost:3000/",//fontend URL
-        methods:["GET","POST"]
+    cors: {
+        origin:"http://localhost:3000",
+        methods:["GET","POST"],
     }
+});
+
+io.on("connection", (socket)=>{
+    console.log(`User Connected: ${socket.id}`);
+    //Join a Particular Room
+    socket.on("join_room", (data) => {
+        socket.join(data);
+      });
+    //Send Message Particularly instaed of Broadcasting
+    socket.on("send_message",(data)=>{
+        socket.to(data.room).emit("received_message", data)
+     // socket.broadcast.emit("received_message", data)
+    })
+
 })
 
-
-app.listen(3001,()=>{
+server.listen(3001,()=>{
     console.log("Server Is Running - Listening to Server")
 }
 )
+
